47efdc89cbc6da9a5cbda4acbd7a6588b41a110d
Admin <Admin@JIGAR>
2025-01-28 02:29:07 +0530

    Add project files.

---
+++ Pages/Shared/Login_Layout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CRM.Pages.Shared
+{
+    public class Login_LayoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
